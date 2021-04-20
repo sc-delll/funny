df324b304ecffeac95be6396f901c71b9502e628
sc-delll <82901274+sc-delll@users.noreply.github.com>
2021-04-20 13:58:46 -0400

    just uploaded the code

---
+++ script.js
@@ -0,0 +1,9 @@
+function music(){
+  var music = new Audio('Chug Jug With You - Parody of American Boy (Number One Victory Royale).mp3');
+ music.play();}
+ function music2(){
+  var music = new Audio('SpongeBob theme song Earrape.mp3');
+ music.play();}
+ function music3(){
+  var music = new Audio('MINE DIAMONDS miNECRAFT PARODY OF TAKE ON ME.mp3');
+ music.play();}
